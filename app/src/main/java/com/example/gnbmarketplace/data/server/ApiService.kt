77e78f5c39c3e30142c8f752fe251fe607cd5997
David <apadavid@gmail.com>
2021-06-19 16:59:46 +0200

    Api service interface and response wrapper added. Base result finished

--- app/src/main/java/com/example/gnbmarketplace/data/server/ApiService.kt
+++ app/src/main/java/com/example/gnbmarketplace/data/server/ApiService.kt
@@ -1,2 +1,22 @@
 package com.example.gnbmarketplace.data.server
 
+import com.example.gnbmarketplace.domain.models.conversion.Conversion
+import com.example.gnbmarketplace.domain.models.product.Product
+import retrofit2.Response
+import retrofit2.http.GET
+
+interface ApiService {
+
+    companion object Factory {
+        const val GET_TRANSACTIONS = "transactions.json"
+        const val GET_CONVERSIONS = "rates.json"
+    }
+
+    @GET(GET_TRANSACTIONS)
+    suspend fun postSignin(): Response<WrappedListResponse<Product>>
+
+    @GET(GET_CONVERSIONS)
+    suspend fun getBanks(): Response<WrappedListResponse<Conversion>>
+
+
+}
--- app/src/main/java/com/example/gnbmarketplace/data/server/ResponseWrapper.kt
+++ app/src/main/java/com/example/gnbmarketplace/data/server/ResponseWrapper.kt
@@ -1,2 +1,11 @@
 package com.example.gnbmarketplace.data.server
 
+import com.google.gson.annotations.SerializedName
+
+data class WrappedListResponse<T> (
+    var code: Int,
+    @SerializedName("message") var message : String,
+    @SerializedName("status") var status : Boolean,
+    @SerializedName("errors") var errors : List<String>? = null,
+    @SerializedName("") var data : List<T>? = null
+)
