1687a62a9bad737dc8d36a1a3652f7d325475967
David <apadavid@gmail.com>
2021-06-22 22:35:00 +0200

    image and show details function added in rv, product details dialog created and implemented in main activity

--- app/src/main/java/com/example/gnbmarketplace/presentation/ProductDetailsFragment.kt
+++ app/src/main/java/com/example/gnbmarketplace/presentation/ProductDetailsFragment.kt
@@ -1,4 +1,77 @@
 package com.example.gnbmarketplace.presentation
 
-class DetailsFragment {
+import android.os.Bundle
+import android.view.LayoutInflater
+import com.example.gnbmarketplace.R
+import com.squareup.picasso.Picasso
+import kotlinx.android.synthetic.main.product_details_dialog.*
+
+class ProductDetailsFragment : androidx.fragment.app.DialogFragment() {
+
+    private lateinit var productImage: String
+    private lateinit var productName: String
+    private var productAmount: Int = 0
+    private var productTotalCost: Float = 0f
+
+    companion object {
+
+        private const val ARG_AMOUNT = "amount"
+        private const val ARG_PROD_NAME = "name"
+        private const val ARG_COST = "mandatory"
+        private const val ARG_IMAGE = "image"
+        const val ARG_TAG = "productDetailFragment"
+
+
+        fun newInstance(amount: Int, productName: String, totalCost: Float, img: String): ProductDetailsFragment {
+            val productsDetailFragment = ProductDetailsFragment()
+            val bundle = Bundle()
+
+            bundle.putInt(ARG_AMOUNT, amount)
+            bundle.putString(ARG_PROD_NAME, productName)
+            bundle.putFloat(ARG_COST, totalCost)
+            bundle.putString(ARG_IMAGE, img)
+
+            productsDetailFragment.arguments = bundle
+
+            return productsDetailFragment
+
+        }
+    }
+
+
+    override fun onCreateView(inflater: LayoutInflater, container: android.view.ViewGroup?, savedInstanceState: Bundle?): android.view.View? {
+        return inflater.inflate(R.layout.product_details_dialog, container, false)
+    }
+
+    override fun onViewCreated(view: android.view.View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        arguments?.let {
+            productName = it.getString(ARG_AMOUNT).toString()
+            productTotalCost = it.getFloat(ARG_COST)
+            productImage = it.getString(ARG_IMAGE).toString()
+            productAmount = it.getInt(ARG_AMOUNT)
+        }
+
+        tv_product_name.text = productName
+
+        tv_product_amount.text = getString(R.string.sample_text)+productAmount
+
+        tv_product_cost.text = getString(R.string.sample_text)+productTotalCost
+
+        Picasso.get().load(productImage).into(iv_product_dialog)
+
+        btn_buy_product.text = getString(R.string.sample_text)
+
+        btn_buy_product.setOnClickListener {
+            dismiss()
+        }
+
+        ib_close_product_dilog.setOnClickListener{
+            dismiss()
+        }
+
+    }
+
+
 }
--- app/src/main/java/com/example/gnbmarketplace/presentation/ProductsAdapter.kt
+++ app/src/main/java/com/example/gnbmarketplace/presentation/ProductsAdapter.kt
@@ -3,16 +3,20 @@ package com.example.gnbmarketplace.presentation
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ImageView
 import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
 import com.example.gnbmarketplace.R
 import com.example.gnbmarketplace.domain.models.product.ProductEntity
+import com.squareup.picasso.Picasso
 
-class ProductsAdapter(var products: List<ProductEntity>): RecyclerView.Adapter<ProductsAdapter.ViewHolder>() {
+class ProductsAdapter(var products: List<ProductEntity>,
+                        var imgs: ArrayList<String>,
+                        var onClick: (ProductEntity) -> Unit): RecyclerView.Adapter<ProductsAdapter.ViewHolder>() {
 
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
-        return ViewHolder(LayoutInflater.from(parent.context).inflate(R.layout.fragment_product, parent,false))
+        return ViewHolder(LayoutInflater.from(parent.context).inflate(R.layout.item_product, parent,false))
     }
 
     override fun getItemCount(): Int {
@@ -28,9 +32,14 @@ class ProductsAdapter(var products: List<ProductEntity>): RecyclerView.Adapter<P
     inner class ViewHolder(view: View): RecyclerView.ViewHolder(view) {
 
         private val tvProductSku: TextView =    itemView.findViewById(R.id.tv_product_title)
+        private val ivProduct: ImageView = itemView.findViewById(R.id.iv_product)
 
         fun onBind(product: ProductEntity, position: Int) {
             tvProductSku.text = product.sku
+            Picasso.get().load(imgs[position]).into(ivProduct)
+            ivProduct.setOnClickListener {
+                onClick(product)
+            }
         }
     }
 }
